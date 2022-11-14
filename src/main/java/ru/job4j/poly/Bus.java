51a4e4ee5a64e3df3c9d65adc2271910c1095a7e
Vadim <vad18ru@gmail.com>
2022-11-14 14:53:27 +0400

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+﻿package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void go() {
+        System.out.println("Автобус едет");
+    }
+
+    @Override
+    public void passergers(int num) {
+        System.out.println("Количество пассажиров " + num);
+    }
+
+    @Override
+    public Double toFill(int kol) {
+        return kol * 31.5;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+﻿package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+
+    void passergers(int num);
+
+    Double toFill(int kol);
+}
